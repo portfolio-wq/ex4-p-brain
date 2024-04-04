a7e5429e9a89e784aea57de4eb1887ad98978a9e
portfolio-wq <spotib2@gmail.com>
2024-04-04 14:52:55 +0000

    remove img

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,11 +11,11 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 justify-center lg:justify-normal">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative rounded-full ">
-          <Image src={hero} alt="" fill className="object-contain" />
-        </div>
+        {/* <div className="h-[50%] lg:h-full lg:w-1/2 relative rounded-full -z-10  overflow-hidden">
+          <Image src={hero} alt="" fill />
+        </div> */}
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
